184631a29b2bf4d50496fd288a5496e93f20f359
LudwigEffort <dr.feynman65@gmail.com>
2022-11-14 23:38:42 +0100

    remake index and css

--- js/script.js
+++ js/script.js
@@ -9,3 +9,9 @@ Consigli del giorno:
 - regola d'oro: riciclare ovunque possibile! Questo significa che per la parte di markup possiamo recuperare html e css dell'esercizio svolto qualche giorno fa: è già tutto pronto!
 - il riciclo spesso va a braccetto con le funzioni! Sapendole sfruttare bene, l'esercizio si riduce a poche righe ;)
  */
+
+var app = new Vue (
+  {
+    el: '#root',
+  }
+);
